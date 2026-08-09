512fd98a5517055759820d393ee22f8b521e5a01
codecuriouscat <mer.djebbar@gmail.com>
2026-08-09 20:14:01 +0100

    day two, level 1 and half of level 2 exercises

---
+++ day_2/variables.py
@@ -0,0 +1,38 @@
+# Day 2: 30 days of python programming
+
+first_name = 'Jane'
+last_name = 'Doe'
+full_name = 'Ibrahim Maza'
+country ='Algeria'
+city='Cherchell'
+age=45
+year=2010
+is_married=False
+is_true = True
+is_light_on = True
+first_name,last_name, full_name= 'Jane','Doe','Ibrahim Maza'
+
+
+# Level 2
+ 
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+
+print(len(first_name))
+
+num_one=5
+num_two=4
+res_sum= num_one+num_two
+
+print(res_sum)
+
+ # i'm tired, will finish later
